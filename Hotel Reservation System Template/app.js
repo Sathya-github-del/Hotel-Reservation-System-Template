96cf3920f57a8de05c066cabf0f29b64279d44bb
Sathya Prakash J <75614237+Sathya-github-del@users.noreply.github.com>
2024-04-25 13:12:36 +0530

    Add files via upload

---
+++ Hotel Reservation System Template/app.js
@@ -0,0 +1,7 @@
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('active');
+  navMenu.classList.toggle('show');
+});
