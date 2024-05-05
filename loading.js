1f596835e1e299fcf6c6ce1def9a0ba89e6adeb3
Sathya Prakash J <75614237+Sathya-github-del@users.noreply.github.com>
2024-05-05 04:27:51 -0700

    Delete loading.js

--- loading.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.querySelector(".loading-screen").style.display = "none";
-        document.querySelector("main").style.display = "block";
-    }, 2000);
-});
